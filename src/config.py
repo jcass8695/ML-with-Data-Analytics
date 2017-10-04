1b1264381c5287be87cb17a1d7082068802138b7
Jack <j.cassidy45@gmail.com>
2017-10-04 18:37:00 +0100

    First Commit

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# File to hold hardcoded constants. Examples might include:
+# Paths
+# Error messages
+# URL's
+# Max/Min values etc..
+
+# All paths are relative to src directory
+PATHS = {
+}
+
+ERRORS = {
+
+}
+
+MAX_VALUES = {
+}
+
+MIN_VALUES = {
+
+}
