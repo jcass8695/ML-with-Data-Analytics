9427e4ad6f5515b01422ef72300ef6ae3bb11b88
Jack <j.cassidy45@gmail.com>
2017-10-13 19:15:44 +0100

    Place config.py in the same directory as source code for easy imports
    Until I figure out how to get src directory to act as a package using __init__.py so we can import config from src, just going to have to include copies of config.py in each task directory.

---
+++ Assignment1/src/task 2/config.py
@@ -0,0 +1,22 @@
+# File to hold hardcoded constants. Examples might include:
+# Paths
+# Error messages
+# URL's
+# Max/Min values etc..
+
+# All paths are relative to task directory
+PATHS = {
+    "SUMDATA_NOISE": "../../../data/The SUM dataset, with noise.csv",
+    "SUMDATA_NOISELESS": "../../data/The SUM dataset, without noise.csv"
+}
+
+ERRORS = {
+
+}
+
+MAX_VALUES = {
+}
+
+MIN_VALUES = {
+
+}
