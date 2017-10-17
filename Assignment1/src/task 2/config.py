ff9c6747cf361717e0c0f4e1252eaedafe5dd848
Jack Cassidy <j.cassidy45@gmail.com>
2017-10-17 14:44:11 +0100

    Move hardcoded CSV columns for SUM dataset to config file

--- Assignment1/src/task 2/config.py
+++ Assignment1/src/task 2/config.py
@@ -6,6 +6,10 @@
  Max/Min values etc..
 '''
 
+CSV_COLUMNS_SUM = ['Instances', 'Feature_1', 'Feature_2', 'Feature_3', 'Feature_4',
+                   'Feature_5', 'Feature_6', 'Feature_7', 'Feature_8', 'Feature_9',
+                   'Feature_10', 'Target', 'Target_Class']
+
 
 # All paths are relative to task directory
 PATHS = {
